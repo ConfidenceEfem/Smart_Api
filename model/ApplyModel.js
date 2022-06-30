9fea193b63aff7bc51c469c08cf3130d1e8655d1
ConfidenceEfem <confidenceefem1@gmail.com>
2022-06-30 02:04:28 +0100

    first commit

---
+++ model/ApplyModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ApplySchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    cvImage: {
+      type: String,
+      required: true,
+    },
+    applicationLetter: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'jobs',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('apply', ApplySchema);
